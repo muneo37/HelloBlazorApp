5e52c040c2e05f64082ad2f918c0491ed7777158
muneo37 <y.wai.ysuke0822@gmail.com>
2022-08-01 13:45:06 +0900

    parametorを使った数値指定

---
+++ HelloBlazorApp/BmiItem.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace HelloBlazorApp
+{
+    public class BmiItem
+    {
+        public string Name { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+        public double Bmi
+        {
+            get=>Height == 0 ? 0 : Weight /((Height/100.0)*(Height/100.0));
+        }
+    }
+}
